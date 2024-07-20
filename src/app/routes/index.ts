8331f64e2af48a6cca24899a30e4e71d18564b17
gharamiananda <anandagharami@gmail.com>
2024-07-20 07:22:36 +0530

    blog

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { AuthRoutes } from "../modules/auth/auth.route";
 import { ProductRoutes } from "../modules/product/product.route";
 import { SalesRoutes } from "../modules/sales/sales.route";
+import { BlogRoutes } from "../modules/blog/blog.route";
+import { ProjectRoutes } from "../modules/project/project.route";
 
 const router = Router();
 
@@ -18,6 +20,13 @@ const moduleRoutes = [
   {
     path: "/sales",
     route: SalesRoutes,
+  },
+  {
+    path: "/blogs",
+    route: BlogRoutes,
+  }, {
+    path: "/projects",
+    route: ProjectRoutes,
   },
 
   
